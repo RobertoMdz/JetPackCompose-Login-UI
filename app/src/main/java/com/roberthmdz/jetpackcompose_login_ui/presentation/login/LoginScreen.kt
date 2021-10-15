c3f2c3104c5d5d79e15924f0735dc376e0ba21a9
RobertoMdz <roberto.cr.mendoza@gmail.com>
2021-10-14 22:14:59 -0500

    Implement log in design

--- app/src/main/java/com/roberthmdz/jetpackcompose_login_ui/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/roberthmdz/jetpackcompose_login_ui/presentation/login/LoginScreen.kt
@@ -1,25 +1,51 @@
 package com.roberthmdz.jetpackcompose_login_ui.presentation.login
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.roberthmdz.jetpackcompose_login_ui.R
+import com.roberthmdz.jetpackcompose_login_ui.presentation.components.RoundedButton
+import com.roberthmdz.jetpackcompose_login_ui.presentation.components.TransparentTextField
 
 @Composable
 fun LoginScreen() {
+    val emailValue = rememberSaveable{ mutableStateOf("")}
+    val passwordValue = rememberSaveable{ mutableStateOf("")}
+    var passwordVisibility by remember { mutableStateOf(false)}
+    val focusManager = LocalFocusManager.current
 
     Box(
         modifier = Modifier
@@ -51,11 +77,10 @@ fun LoginScreen() {
                         topStartPercent = 8,
                         topEndPercent = 8,
                     )
-
                 ) {
 
                     Column(
-                        modifier = Modifier.padding(16.dp),
+                        modifier = Modifier.fillMaxWidth().padding(16.dp),
                         verticalArrangement = Arrangement.SpaceEvenly
                     ) {
                         Text(
@@ -64,33 +89,105 @@ fun LoginScreen() {
                                 fontWeight = FontWeight.Medium
                             )
                         )
+
                         Text(
                             text = "Login to your account",
                             style = MaterialTheme.typography.h5.copy(
                                color = MaterialTheme.colors.primary
                             )
                         )
-                    }
 
-                    Column(
-                        modifier = Modifier.padding(horizontal = 16.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.spacedBy(8.dp)
-                    ) {
-                        Text(
-                            text = "Wellcome Back!",
-                            style = MaterialTheme.typography.h4.copy(
-                                fontWeight = FontWeight.Medium
+                        Column(
+                            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            TransparentTextField(
+                                textFieldValue = emailValue,
+                                textLabel = "Email",
+                                keyboardType = KeyboardType.Email,
+                                keyboardActions = KeyboardActions(
+                                    onNext = {
+                                        focusManager.moveFocus(FocusDirection.Down)
+                                    }
+                                ),
+                                imeAction = ImeAction.Next
                             )
-                        )
-                        Text(
-                            text = "Login to your account",
-                            style = MaterialTheme.typography.h5.copy(
-                                color = MaterialTheme.colors.primary
-                            )
-                        )
-                    }
 
+                            TransparentTextField(
+                                textFieldValue = passwordValue,
+                                textLabel = "Password",
+                                keyboardType = KeyboardType.Password,
+                                keyboardActions = KeyboardActions(
+                                    onDone = {
+                                        focusManager.clearFocus()
+                                        // TODO: Implement login
+                                    }
+                                ),
+                                imeAction = ImeAction.Done,
+                                trailingIcon = {
+                                    IconButton(
+                                        onClick = {
+                                            passwordVisibility = !passwordVisibility
+                                        }
+                                    ) {
+                                        Icon(
+                                            imageVector = if(passwordVisibility) {
+                                                Icons.Default.Visibility
+                                            } else {
+                                                Icons.Default.VisibilityOff
+                                            },
+                                            contentDescription = "Password"
+                                        )
+                                    }
+                                },
+                                visualTransformation = if(passwordVisibility) {
+
+                                    VisualTransformation.None
+                                } else {
+                                    PasswordVisualTransformation()
+                                }
+                            )
+
+                            Text(
+                                modifier = Modifier.fillMaxWidth(),
+                                text = "Forgot password?",
+                                style = MaterialTheme.typography.body1,
+                                textAlign = TextAlign.End
+                            )
+                        }
+
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+
+                            RoundedButton(
+                                text = "Log in",
+                                displayProgressBar = false,
+                                onClick = {
+                                    //TODO: Init Login
+                                }
+                            )
+
+                            ClickableText(
+                                text = buildAnnotatedString {
+                                    append("Do not have an account?")
+                                    withStyle(
+                                        style = SpanStyle(
+                                            color = MaterialTheme.colors.primary,
+                                            fontWeight = FontWeight.Bold
+                                        )
+                                    ) {
+                                        append("Sign up")
+                                    }
+                                },
+                            ) {
+                                //TODO: Navigate to register screen
+                            }
+                        }
+                    }
                 }
 
                 FloatingActionButton(
